666d5d7843d7f3173515a47b41362e08e1dc77f7
sagarpaudell <sp02434118@student.ku.edu.np>
2022-04-15 23:16:54 +0545

    basic auth completed

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:votiface/screens/account/account_screen.dart';
+import 'package:votiface/screens/landing/landing_screen.dart';
 import 'package:votiface/screens/landing/main_screen.dart';
 import 'package:votiface/screens/vote/vote_screen.dart';
 
 import 'screens/auth/auth_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  LandingScreen.routeName: (ctx) => LandingScreen(),
   MainScreen.routeName: (ctx) => MainScreen(),
   AuthScreen.routeName: (ctx) => AuthScreen(),
   VoteScreen.routeName: (ctx) => VoteScreen(),
--- lib/screens/auth/components/body.dart
+++ lib/screens/auth/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
+import 'login_widget.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -10,7 +11,7 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         Container(
-          height: MediaQuery.of(context).size.height * 0.30,
+          height: MediaQuery.of(context).size.height * 0.40,
           width: double.infinity,
           child: Center(
             child: Text(
@@ -23,114 +24,7 @@ class Body extends StatelessWidget {
             ),
           ),
         ),
-        Expanded(
-          child: SingleChildScrollView(
-            child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.7,
-              decoration: const BoxDecoration(
-                  color: backColor,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(40.0),
-                    topLeft: Radius.circular(40.0),
-                  )),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 22),
-                    child: Text(
-                      'Login',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Card(
-                    margin: EdgeInsets.fromLTRB(22, 10, 40, 10),
-                    child: Container(
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Voter ID',
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                          prefixIcon: Icon(Icons.account_circle),
-                        ),
-                        textInputAction: TextInputAction.next,
-                        keyboardType: TextInputType.text,
-                      ),
-                    ),
-                  ),
-                  Card(
-                    margin: EdgeInsets.fromLTRB(22, 10, 40, 10),
-                    child: Container(
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                            hintText: 'Password',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10),
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10),
-                              ),
-                            ),
-                            prefixIcon: Icon(Icons.lock)),
-                        textInputAction: TextInputAction.next,
-                        keyboardType: TextInputType.text,
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Forget Password?  ',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: kPrimaryColor,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Center(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      width: 200,
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Login',
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(80),
-                          color: kPrimaryColor),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+        LoginWidget(),
       ],
     );
   }
