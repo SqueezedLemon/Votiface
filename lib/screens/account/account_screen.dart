4db6b3d92d41cc9ac873573e32f393a010ed1ea9
buddha gautam <buddhagautam231@gmail.com>
2022-04-16 13:35:29 +0545

    remove test blockchain code

--- lib/screens/account/account_screen.dart
+++ lib/screens/account/account_screen.dart
@@ -15,91 +15,91 @@ class AccountScreen extends StatefulWidget {
 }
 
 class _AccountScreenState extends State<AccountScreen> {
-  String privateKey =
-      '614aff264350728eed060541370810b678d49482bd6f957ce200d93ba18d51b2';
-  // String rpcUrl = 'http://192.168.0.109:8545';
-  String rpcUrl = 'https://kovan.infura.io/v3/5eddb680b6cf4ea0936f900b9269b4e9';
-  String contractAddress = "0x0da0A4EC7333aad77E6630340E0f5B791525f5AE";
-  late Client httpClient;
-  late Web3Client ethereumClient;
-  String contractName = "Voting";
+  // String privateKey =
+  //     '614aff264350728eed060541370810b678d49482bd6f957ce200d93ba18d51b2';
+  // // String rpcUrl = 'http://192.168.0.109:8545';
+  // String rpcUrl = 'https://kovan.infura.io/v3/5eddb680b6cf4ea0936f900b9269b4e9';
+  // String contractAddress = "0x0da0A4EC7333aad77E6630340E0f5B791525f5AE";
+  // late Client httpClient;
+  // late Web3Client ethereumClient;
+  // String contractName = "Voting";
 
-  int voters_count = 0;
-  List<dynamic> candidates = [[]];
+  // int voters_count = 0;
+  // List<dynamic> candidates = [[]];
 
-  @override
-  void initState() {
-    super.initState();
-    httpClient = Client();
-    ethereumClient = Web3Client(rpcUrl, httpClient);
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   httpClient = Client();
+  //   ethereumClient = Web3Client(rpcUrl, httpClient);
+  // }
 
-  Future<void> votersCount() async {
-    setState(() {});
-    List<dynamic> result = await query('voters_count', []);
-    print(result.length);
-    voters_count = int.parse(result[0].toString());
-    print(voters_count.toString());
-    setState(() {});
-  }
+  // Future<void> votersCount() async {
+  //   setState(() {});
+  //   List<dynamic> result = await query('voters_count', []);
+  //   print(result.length);
+  //   voters_count = int.parse(result[0].toString());
+  //   print(voters_count.toString());
+  //   setState(() {});
+  // }
 
-  Future<void> getCandidates() async {
-    setState(() {});
-    List<dynamic> result = await query('getCandidates', []);
-    print(result[0]);
-    candidates = result[0];
-    print(candidates);
+  // Future<void> getCandidates() async {
+  //   setState(() {});
+  //   List<dynamic> result = await query('getCandidates', []);
+  //   print(result[0]);
+  //   candidates = result[0];
+  //   print(candidates);
 
-    setState(() {});
-  }
+  //   setState(() {});
+  // }
 
-  Future<DeployedContract> getContract() async {
-    String abi = await rootBundle.loadString("assets/blockchain/abi.json");
+  // Future<DeployedContract> getContract() async {
+  //   String abi = await rootBundle.loadString("assets/blockchain/abi.json");
 
-    DeployedContract contract = DeployedContract(
-      ContractAbi.fromJson(abi, contractName),
-      EthereumAddress.fromHex(contractAddress),
-    );
+  //   DeployedContract contract = DeployedContract(
+  //     ContractAbi.fromJson(abi, contractName),
+  //     EthereumAddress.fromHex(contractAddress),
+  //   );
 
-    return contract;
-  }
+  //   return contract;
+  // }
 
-  Future<List<dynamic>> query(String functionName, List<dynamic> args) async {
-    DeployedContract contract = await getContract();
-    ContractFunction function = contract.function(functionName);
-    List<dynamic> result = await ethereumClient.call(
-        contract: contract, function: function, params: args);
-    return result;
-  }
+  // Future<List<dynamic>> query(String functionName, List<dynamic> args) async {
+  //   DeployedContract contract = await getContract();
+  //   ContractFunction function = contract.function(functionName);
+  //   List<dynamic> result = await ethereumClient.call(
+  //       contract: contract, function: function, params: args);
+  //   return result;
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: ElevatedButton(
-              onPressed: () {
-                votersCount();
-              },
-              child: Text("total Voters")),
-        ),
-        Text(
-          voters_count.toString(),
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        ElevatedButton(
-            onPressed: () {
-              getCandidates();
-            },
-            child: Text("total_candidates")),
-        Text(
-          candidates.toString(),
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ],
-    );
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     Align(
+        //       alignment: Alignment.center,
+        //       child: ElevatedButton(
+        //           onPressed: () {
+        //             votersCount();
+        //           },
+        //           child: Text("total Voters")),
+        //     ),
+        //     Text(
+        //       voters_count.toString(),
+        //       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        //     ),
+        //     ElevatedButton(
+        //         onPressed: () {
+        //           getCandidates();
+        //         },
+        //         child: Text("total_candidates")),
+        //     Text(
+        //       candidates.toString(),
+        //       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        //     ),
+        //   ],
+        );
   }
 }
