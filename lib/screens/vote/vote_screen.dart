7905f122cca843e1bf030795393e2530a6f6b817
Biraj Adhikari <birajkmc4@gmail.com>
2022-05-04 21:37:59 +0545

    card added

--- lib/screens/vote/vote_screen.dart
+++ lib/screens/vote/vote_screen.dart
@@ -1,10 +1,15 @@
 import 'dart:ui';
-
+import '../vote/components/body.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:votiface/constants.dart';
+import 'package:flutter/material.dart';
 
 enum currectTab { active, upcoming }
+
+var heading = 'National Election';
+var subheading = '2022';
+var supportingText = '5:45';
 
 class VoteScreen extends StatefulWidget {
   static const routeName = '/vote';
@@ -90,11 +95,51 @@ class _VoteScreenState extends State<VoteScreen> {
               ),
             ],
           ),
-          SizedBox(width: double.infinity, height: kDefaultPadding),
           Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(5),
               color: kTextColor,
+            ),
+          ),
+          Container(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              elevation: 10.0,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(heading),
+                    subtitle: Text(subheading),
+                  ),
+                  SizedBox(
+                      height: 250.0,
+                      child: Image.asset(
+                        "assets/ElectionBanner.png",
+                        fit: BoxFit.contain,
+                      )),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    alignment: Alignment.centerLeft,
+                    child: Text(supportingText),
+                  ),
+                  ButtonBar(
+                    children: [
+                      TextButton(
+                        child: const Text(
+                          'Vote',
+                          style: const TextStyle(fontSize: 26),
+                        ),
+                        onPressed: () {/* ... */},
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
             ),
           ),
         ],
